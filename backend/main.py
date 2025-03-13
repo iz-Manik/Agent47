1f9c9073f8e6f9dfdd6a0f07ad4110677f963da0
Manik <manikchauhan029@gmail.com>
2025-03-13 15:13:05 +0530

    Committed backend/main.py

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from scraper import fetch_news
+from summarizer import summarize_article
+from tone_changer import change_tone
+from fastapi.middleware.cors import CORSMiddleware
+import traceback
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Security-Policy"],
+)
+
+@app.middleware("http")
+async def add_security_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Content-Security-Policy"] = "script-src 'self' 'unsafe-inline' 'unsafe-eval' data:"
+    return response
+
+@app.get("/news")
+def get_news(tone: str = "neutral"):
+    try:
+        urls = fetch_news()  # Get only URLs from the scraper
+        processed_news = []
+
+        for url in urls:
+            title,summary = summarize_article(url)
+            final_text = change_tone(summary, tone)
+
+            processed_news.append({
+                "title": title,
+                "summary": final_text,
+                "url": url
+            })
+
+        return {"news": processed_news}
+    except Exception as e:
+        traceback.print_exc()
+        return {"error": str(e)}
+
+# Start server with: uvicorn main:app --reload
